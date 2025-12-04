7adb1f860dd968cb38b8b2de3dd194ce8ef18c1c
yuriEGV <yguajardov@gmail.com>
2025-12-04 10:20:12 -0300

    Actualización del proyecto

--- api/index.js
+++ api/index.js
@@ -1,28 +1,26 @@
+// api/index.js
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import morgan from "morgan";
 import connectDB from "../config/db.js";
 import routes from "../routes/index.js";
 
+// Cargar variables de entorno desde .env en la raíz del proyecto
 dotenv.config();
 
 const app = express();
-
-// Middlewares
 app.use(cors());
 app.use(express.json());
-app.use(morgan("dev"));
 
-// Conectar Mongo
+// Conectar a MongoDB
 connectDB();
 
 // Rutas principales
 app.use("/api", routes);
 
-// Ruta de prueba raíz
+// Ruta básica de prueba
 app.get("/", (req, res) => {
-  res.json({ message: "Einflix API funcionando desde Vercel" });
+  res.json({ message: "Einflix API funcionando" });
 });
 
 export default app;
