0b7beb5a2e9eed989427d9b75c0fb13c6170c309
yuriEGV <yguajardov@gmail.com>
2025-12-16 10:09:14 -0300

    Actualización del proyecto

---
+++ einflix-backend(Express)/server.js
@@ -0,0 +1,10 @@
+// server.js
+import dotenv from "dotenv";
+import app from "../api/index.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
